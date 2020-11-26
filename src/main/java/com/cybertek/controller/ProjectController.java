3b69c6c70f9bd3d1855cdb91eca476855241e3a2
Gulhan Karali Balci <gulhan.karali@gmail.com>
2020-11-26 00:31:34 +0000

    version 2

--- src/main/java/com/cybertek/controller/ProjectController.java
+++ src/main/java/com/cybertek/controller/ProjectController.java
@@ -1,9 +1,7 @@
 package com.cybertek.controller;
 
 import com.cybertek.dto.ProjectDTO;
-import com.cybertek.dto.UserDTO;
 import com.cybertek.service.ProjectService;
-import com.cybertek.service.RoleService;
 import com.cybertek.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
@@ -31,17 +29,15 @@ public class ProjectController {
         model.addAttribute("project", new ProjectDTO());
         model.addAttribute("projectList", projectService.findAll());
         model.addAttribute("userList", userService.findAll());
-        return "project/projectCrud";
+        return "project/create";
     }
 
     @PostMapping("/create")
-    public String postProject(@ModelAttribute("project") ProjectDTO project, Model model) {
+    public String postProject(@ModelAttribute("project") ProjectDTO project) {
         projectService.save(project);
-        model.addAttribute("project", new ProjectDTO());
-        model.addAttribute("projectList", projectService.findAll());
-        model.addAttribute("userList", userService.findAll());
-        return "project/projectCrud";
+        return "redirect:/project/create";
     }
+
 
     @GetMapping("/create/delete/{id}")
     public String deleteproject(@PathVariable("id") String id, Model model) {
@@ -49,6 +45,6 @@ public class ProjectController {
         model.addAttribute("project", new ProjectDTO());
         model.addAttribute("projectList", projectService.findAll());
         model.addAttribute("userList", userService.findAll());
-        return "project/projectCrud";
+        return "project/create";
     }
 }
